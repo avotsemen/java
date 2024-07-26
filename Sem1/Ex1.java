7bb552b4dda565da9169ed118448fc6d248a2218
Mamaev Semyon <avot.semen@gmail.com>
2024-07-26 20:50:27 +0500

    Перенес решение задачи в папку Sem1 и переименовал

---
+++ Sem1/Ex1.java
@@ -0,0 +1,31 @@
+package Sem1;
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // In: n = 234
+        // Out: 15
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int n = scanner.nextInt();
+        int result = getSumMinusPr(n);
+
+        System.out.println("result: " + result);
+    }
+    
+/**
+ * 
+ * @param n начальное значение
+ * @return произведение минус сумма
+ */
+    private static int getSumMinusPr(int n) {
+        int sum = 0;
+        int pr = 1;
+        while (n != 0) {
+            sum += n % 10;
+            pr *= n % 10;
+            n /= 10;
+        }
+        return pr - sum;
+    }
+}
