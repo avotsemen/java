05d46b127be2f849e482815e74f6ceba7f855f8f
Mamaev Semyon <avot.semen@gmail.com>
2024-07-28 00:57:04 +0500

    Семинар 2. Задача 4. Добавили логгер

--- Sem2/Ex4.java
+++ Sem2/Ex4.java
@@ -3,11 +3,17 @@ package Sem2;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 // Напишите метод, который составит строку, состоящую из 100
 // повторений слова TEST и метод, который запишет эту строку в
 // простой текстовый файл, обработайте исключения.
 public class Ex4 {
+
+    private static final Logger logger = Logger.getLogger(Ex4.class.getName());
     public static void main(String[] args) throws Exception {
         String str = "TEST\n".repeat(100);
         writeToFile(str, "test.txt");
@@ -18,6 +24,10 @@ public class Ex4 {
             bw.write(content);
             System.out.println("Строка успешно записана в файл " + fileName);
         } catch (IOException e) {
+            FileHandler fh = new FileHandler("error.log", true);
+            logger.addHandler(fh);
+            fh.setFormatter(new SimpleFormatter());
+            logger.log(Level.WARNING, "Ошибка записи");
             System.out.println("Ошибка записи в файл " + fileName);
         }
     }
